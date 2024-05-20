7b6d9920754c4faeb1dbe2e9e212218f0a817ac2
Gerson Bernardo <gbds25@gmail.com>
2024-05-20 12:03:59 -0300

    Estudo JavaScript Parte 01

---
+++ 06-JavaScript/06.01-Introducao/js/script.js
@@ -0,0 +1,1 @@
+console.log("Vendo o segundo modo de interação no código")
---
+++ 06-JavaScript/06.02-Tipos_de_dados/js/script.js
@@ -0,0 +1,31 @@
+// 01 - Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+// 02 - Operações aritméticas
+console.log(2+4); 
+console.log(10-5); 
+console.log(2*4); 
+console.log(2/4); 
+console.log((5+2)*3);
+
+// 03 - special numbers
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
+console.log(2* "abc")
+
+// 04 - Strings
+console.log("Um Texto");
+console.log('Um Texto');
+console.log(`Um Texto`);
+
+console.log(typeof "Um Texto");
+console.log(typeof 'Um Texto');
+console.log(typeof `Um Texto`);
+
+console.log("Gerson Bernardo da silva\n R: José Vicente de Barros 1762")
+console.log('Espaçamento de \t tab')
+
+console.log("Meu"+' Nome é:'+` Gerson Bernardo`);
