cdfe6ae97bc0bb54e8315666e35b8631a616395f
Gerson Bernardo <gbds25@gmail.com>
2024-05-21 10:59:00 -0300

    Estudando JavaScript Parte 02

--- 06-JavaScript/06.02-Tipos_de_dados/js/script.js
+++ 06-JavaScript/06.02-Tipos_de_dados/js/script.js
@@ -25,7 +25,45 @@ console.log(typeof "Um Texto");
 console.log(typeof 'Um Texto');
 console.log(typeof `Um Texto`);
 
+// 05 - Simbolos especiais em string
 console.log("Gerson Bernardo da silva\n R: José Vicente de Barros 1762")
 console.log('Espaçamento de \t tab')
 
+// 06 - Concatenação
 console.log("Meu"+' Nome é:'+` Gerson Bernardo`);
+
+// 07 - Template string
+console.log(`A soma de 2+2 é: ${2+2}`)
+
+console.log(`Podemos executar qualquer coisa aqui ${console.log("Ola mundo ")}`)
+
+// 08 - Boolean
+console.log(true);
+console.log(false);
+console.log(2 ===2 );
+console.log(5 < 3);
+console.log(typeof false);
+console.log(typeof true);
+
+// 09 - Comparações
+console.log(5 >= 5);
+console.log(5 > 7);
+console.log(10 == 10);
+console.log(10 != 8);
+
+// 10 - Idêntico
+console.log(9=="9");
+console.log(9 + "9");
+console.log(9==="9");
+console.log(9 != "9");
+console.log(9 !== "9");
+
+// 11 - Operadores Lógicos
+console.log(true && true);
+console.log(true && false);
+console.log(5>2 && 2<10);
+console.log(5>3 && "Matheus" === 1);
+console.log(5>3 || "Matheus" === 1);
+console.log(5<3 || 10>11);
+console.log(!true);
+console.log(!5>4);
