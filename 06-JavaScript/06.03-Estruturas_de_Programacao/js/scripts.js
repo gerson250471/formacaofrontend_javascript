28c864aec1b7c401e4f446029fe81aad36333668
Gerson Bernardo <gbds25@gmail.com>
2024-05-23 10:54:27 -0300

    Estudando JavaScript Parte 04

--- 06-JavaScript/06.03-Estruturas_de_Programacao/js/scripts.js
+++ 06-JavaScript/06.03-Estruturas_de_Programacao/js/scripts.js
@@ -39,3 +39,76 @@ let $teste="ok";
 console.log(Math.max(10,2,3,4,24));
 console.log(Math.floor(4.07));
 console.log(Math.ceil(5.04));
+
+// 06 - Console
+console.log("Escreve no console");
+console.error("Mostra o erro no console");
+console.warn("Mostra  avisos no console");
+
+// 07 - if
+const m = 10;
+if( m > 6 ){
+    console.log("M é maior que 5")
+}
+
+const user = "João"
+if (user=="João"){
+    console.log("Saudações João")
+}
+
+if (user == "Maria"){
+    console.log("Saudação Maria")
+}
+
+// 08 - Else
+const loggedin = false;
+if (loggedin){
+    console.log("Usuário Autenticado")
+} else {
+    console.log("Usuário não está Autenticado")
+}
+
+// 09 - else if
+if( 1 > 2 ){
+    console.log("Teste");
+} else if ( 2 > 3 ){
+    console.log("Teste 2");
+} else if ( 5 > 1 ){
+    console.log("Agora sim");
+}
+
+// 10 - while
+let p = 0;
+while (p < 5){
+    console.log("Repetindo " + p)
+    p = p + 1;
+}
+
+//  11 - do while
+let o = 10;
+do{
+    console.log(`Valor de o: ${o}`)
+    o--
+}while(o > 1);
+
+// 12 - for
+for( b = 1; b < 12; b++) {
+    console.log(`Repetindo b: ${b}`);
+}
+
+// 13 - identação
+
+let nome1= "João";
+let nome2= "Maria";
+
+for( b = 1; b < 3 ; b++) {
+    if (b==1){
+        console.log("Meu primeiro");
+    }else if(b==2){
+        if (nome1=="João") {
+            console.log("Este é o meu segundo João");
+        }else{
+            console.log("Este é o meu segundo Maria");
+        }
+    }
+} 
