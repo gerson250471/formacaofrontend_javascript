3a93c4c931e146d2f305bd6420319a395c78b71d
Gerson Bernardo <gbds25@gmail.com>
2024-05-25 10:20:59 -0300

    Estudando JavaScript Parte 06

--- 06-JavaScript/06.04-Funcoes/js/scripts.js
+++ 06-JavaScript/06.04-Funcoes/js/scripts.js
@@ -124,3 +124,86 @@ const saudar = (nome) =>{
 
 saudar();
 saudar("Maria");
+
+// 08 - Valor default
+const customGreeting = (name, greet = "Olá") =>{
+    return `${greet}, ${name}!`;
+};
+
+console.log(customGreeting("Gerson"));
+console.log(customGreeting("Abner","Bom dia"));
+
+const repetirFrase = (frase,repet = 2) =>{
+    for (let a=0;a<repet;a++) {
+        console.log(`${a} ${frase}`)
+    }
+}
+
+repetirFrase("Estou aprendendo");
+repetirFrase("Vou vencer",5);
+
+// 09 - closure
+function someFunction(){
+    let txt="Estou vendo algo";
+
+    function display(){
+        console.log(txt);
+    }
+
+    display();
+}
+
+someFunction();
+
+// 10 - mais sobre closure
+const multiplicationClosure = (n) => {
+    return (m) =>{
+        return n * m ;
+    };
+};
+
+const c1 = multiplicationClosure(5);
+const c2 = multiplicationClosure(6);
+
+console.log(c1);
+console.log(c2);
+console.log(c1(4));
+console.log(c2(8));
+
+// 11 - Recursion
+const untilTen = (n,m) => {
+    if (n<10) {
+        console.log("A função parou a execução");
+    } else {
+        const x =  n - m;
+        console.log(x);
+        untilTen(x,m);
+    }
+};
+ 
+untilTen(112,7);
+
+/* Tomar cuidado para não criar uma recursão infinita
+    ----------------------------------------------
+    EXEMPLO
+    ----------------------------------------------
+    function run(){
+        console.log("Executando . . . ");
+        run();
+    };
+
+    run();
+*/
+
+function factorial(x) {
+    if (x === 0){
+        return 1
+    } else {
+        return x * factorial( x - 1)
+    }
+}
+
+const num = 6;
+const result = factorial(num);
+
+console.log(`O fatorial do número ${num} é : ${result}`);
