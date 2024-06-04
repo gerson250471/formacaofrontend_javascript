69b9b31ab1e33c1085d10e0ed2aa87ef1219150d
Gerson Bernardo <gbds25@gmail.com>
2024-06-04 10:02:16 -0300

    Estudando JavaScript Parte 15

--- 06-JavaScript/06.08-Async/js/script.js
+++ 06-JavaScript/06.08-Async/js/script.js
@@ -39,3 +39,84 @@ Promise.resolve(4 * "asd")
         }
     })
     .catch((err) => console.log(`Ocorreu um erro ${err}`));
+
+// 05 - rejeição
+function verificarNumero(n) {
+    return new Promise((resolve,reject) =>{
+        if (n > 10) {
+            resolve (`O número é maior que 10`);
+        } else {
+            reject(new Error("O número é muito baixo"));
+        }
+    });
+}
+
+const a = verificarNumero(20);
+
+const b = verificarNumero(10);
+
+a.then((v) => console.log (`O resultado é ${v}`)).catch((err) =>
+    console.log(`Um erro ocorreu: ${err}`)
+);
+
+b.then((v) => console.log (`O resultado é ${v}`)).catch((err) =>
+    console.log(`Um erro ocorreu: ${err}`)
+);
+
+// 06 - resolvendo várias promises
+const p1 = new Promise((resolve,reject) => {
+    setTimeout(function (){
+        resolve(10);
+    },3000);
+});
+
+const p2 = Promise.resolve( 10 + 10 );
+
+const p3 = new Promise((resolve,reject) => {
+    if ( 30 > 10 ) {
+        resolve(30);
+    } else {
+        reject("Erro!");
+    }
+});
+
+Promise.all([p1,p2,p3]).then ((values) => console.log(values));
+
+// 07 - async functions
+async function somarComEspera(a,b) {
+    return a + b 
+}
+
+somarComEspera(2,4).then((value) => {
+    console.log(`${a} + ${b} = ${value}`);
+});
+
+// 08 - async await
+function resolveComEspera() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Resolveu a Promise`);
+        },2000);
+    });
+}
+
+async function chamadaAsync() {
+    console.log("Chamando a Promise, e esperando o resultado")
+    const result = await resolveComEspera()
+    console.log(`O resultado chegou: ${result}`);
+}
+
+chamadaAsync();
+
+// 09 - generator
+function* generator() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const gen = generator();
+
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
