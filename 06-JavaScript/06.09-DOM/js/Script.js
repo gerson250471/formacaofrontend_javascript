0f0b18032c9cbd5ef853e4bf3f3cd98984de46bc
Gerson Bernardo <gbds25@gmail.com>
2024-06-05 09:47:29 -0300

    Estudando JavaScript Parte 16

---
+++ 06-JavaScript/06.09-DOM/js/Script.js
@@ -0,0 +1,8 @@
+// 01 - Movendo-se pelo DOM
+console.log(document.body);
+
+console.log(document.body.childNodes);
+
+console.log(document.body.childNodes[1]);
+console.log(document.body.childNodes[1].childNodes);
+console.log(document.body.childNodes[1].childNodes[1].textContent);
