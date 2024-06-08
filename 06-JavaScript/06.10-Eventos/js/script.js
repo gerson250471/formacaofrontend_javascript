7083c920e68b18a78640c16bc6ea006be16dfd57
Gerson Bernardo <gbds25@gmail.com>
2024-06-08 14:14:48 -0300

    Estudando JavaScript Parte 19

--- 06-JavaScript/06.10-Eventos/js/script.js
+++ 06-JavaScript/06.10-Eventos/js/script.js
@@ -19,3 +19,36 @@ thirdBtn.addEventListener("click", () => {
     console.log("Evento foi removido");
     secondbtn.removeEventListener("click",imprimir);
 });
+
+// 03 - argumento do evento
+const myTitle = document.querySelector("#my-title");
+
+myTitle.addEventListener("click",(e) => {
+    console.log(e);
+    console.log(e.offsetX);
+    console.log(e.pointerType);
+    console.log(e.target);
+
+})
+
+// 04 - propagação
+const containerBtn = document.querySelector("#btn-container");
+const btnInsideContainer = document.querySelector("#div-btn");
+
+containerBtn.addEventListener("click", () => {
+    console.log("Evento 1");
+});
+
+btnInsideContainer.addEventListener("click", (e) => {
+    e.stopPropagation();
+    console.log("Evento 2");
+});
+
+// 05 - removendo evento padrão
+const a = document.querySelector("a");
+
+a.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    console.log("Não alterou a página");
+})
