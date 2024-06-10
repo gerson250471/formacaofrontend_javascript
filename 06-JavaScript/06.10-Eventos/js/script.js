fe0e5e22910d77667deea3cb47721ae4c695542f
Gerson Bernardo <gbds25@gmail.com>
2024-06-10 09:42:02 -0300

    Conclusão do Estudo de Javascript

--- 06-JavaScript/06.10-Eventos/js/script.js
+++ 06-JavaScript/06.10-Eventos/js/script.js
@@ -52,3 +52,80 @@ a.addEventListener("click", (e) => {
     
     console.log("Não alterou a página");
 })
+
+// 06 - eventos da tecla
+document.addEventListener("keyup",(e) => {
+    console.log(`Soltou a tecla ${e.key}`);
+});
+
+document.addEventListener("keydown",(e) => {
+    console.log(`Apertou a tecla ${e.key}`);
+});
+
+// 07 - eventos de mouse
+const mouseEventos = document.querySelector("#mouse");
+
+mouseEventos.addEventListener("mousedown", () => {
+    console.log("Apertou o mouse");
+});
+
+mouseEventos.addEventListener("mouseup", () => {
+    console.log("Soltou o mouse");
+});
+
+mouseEventos.addEventListener("dblclick", () => {
+    console.log("Duplo click");
+});
+
+// 08 - movimento do mouse
+document.addEventListener("mousemove", (e) =>{
+    // console.log(`No eixo x ${e.x}`)
+    // console.log(`No eixo y ${e.y}`)
+});
+
+// 09 - eventos scroll
+window.addEventListener("scroll", (e) => {
+    if(window.pageYOffset > 200){
+        console.log("Passei dos 200 px");
+    };
+});
+
+// 10 - evento de focus / blur
+const input = document.querySelector("#my-input")
+
+input.addEventListener("focus", (e) =>{
+    console.log("Entrou no focus");
+});
+
+input.addEventListener("blur", (e) =>{
+    console.log("Saiu do focus");
+});
+
+// 11 - evento de carregamento
+window.addEventListener("load", () => {
+    console.log("Carregou a página")
+});
+
+window.addEventListener("beforeunload", (e) =>{
+    e.preventDefault();
+    e.returnValue = "Teste";
+});
+
+// 12 - debounce
+const debounce = (f,delay) =>{
+    let timeout;
+
+    return (...arguments) =>{
+        if(timeout) {
+            clearTimeout(timeout);
+        }
+
+        timeout = setTimeout(() => {
+            f.apply(arguments);
+        },delay);
+    };
+};
+
+window.addEventListener("mousemove",debounce(() =>{
+    console.log("Executando a cada 400 ms")
+},400));
