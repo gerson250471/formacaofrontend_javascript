f1e2ba30dc4d5be7aabec5a0c27836b23fb0b1ed
Gerson Bernardo <gbds25@gmail.com>
2024-06-12 11:44:07 -0300

    Conclusão do Projeto 06

--- Projeto_06/js/script.js
+++ Projeto_06/js/script.js
@@ -1,1 +1,46 @@
-console.log("estou integrado")
+// Seleção de elementos
+const multiplicationForm = document.querySelector("#multiplication-form")
+const numberInput = document.querySelector("#number")
+const multiplicationInput = document.querySelector("#multiplicator")
+/* obtendo a tabela de multiplicação */
+const multiplicationTable = document.querySelector("#multiplication-operations")
+
+/* Obtendo o span para informar qual é a tabuada executada */
+const multiplicationTitle = document.querySelector("#multiplication-title span")
+
+// Funções
+const createTable = (number,multiplicatorNumber) => {
+    /* Limpar área da Tabuada  */
+    multiplicationTable.innerHTML="";
+
+    /* Gerar a tabuada */ 
+    for(i=1;i<= multiplicatorNumber;i++){
+        const result = number * i;
+        
+        /* Gerar template string */
+        const template = `<div class="row">
+            <div class="operation"> ${number} * ${i} = </div>
+            <div class=result>${result}</div>
+        </div>;`
+
+        const parser = new DOMParser();
+        const htmlTemplate = parser.parseFromString(template,"text/html");
+        const row = htmlTemplate.querySelector(".row");
+        multiplicationTable.appendChild(row);
+    }
+    multiplicationTitle.innerHTML = number;
+};
+
+// Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    // Validar preenchimento dos numeros
+    if(!multiplicationNumber || !multiplicatorNumber) return;
+
+    createTable(multiplicationNumber,multiplicatorNumber);
+
+})
