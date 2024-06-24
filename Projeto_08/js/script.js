dfc0bfb5842398d2c6b84a01c4529be992e00a6d
Gerson Bernardo <gbds25@gmail.com>
2024-06-24 12:20:52 -0300

    Conclusão  do Projeto 08

--- Projeto_08/js/script.js
+++ Projeto_08/js/script.js
@@ -1,1 +1,28 @@
-console.log("setup Inicial")
+/* Selecionar o objeto desejado */
+const buttons = document.querySelectorAll("#image-picker li");
+const image = document.querySelector("#product-image");
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) =>{
+        
+        /* remover seleção do botão */
+        buttons.forEach((btn) => {
+            btn.querySelector(".color").classList.remove("selected")
+        })
+
+        /* Obter o target e o ID do botão selecionado */
+        const button = e.target;
+        const id = button.getAttribute("id");
+        console.log(id);
+
+        /* Colocar a seleção no botão acionado */
+        button.querySelector(".color").classList.add("selected")
+
+        /* Trocar imagem */
+        image.classList.add("changing");
+        image.setAttribute("src",`img/iphone_${id}.jpg`);
+        setTimeout(() => {
+            image.classList.toggle("changing");
+        },200);
+    })
+})
