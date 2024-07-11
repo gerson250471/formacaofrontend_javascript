8a76a31f93d51cfcbd2f37e3c8bb1ef2b4028ba4
Gerson Bernardo <gbds25@gmail.com>
2024-07-11 12:15:28 -0300

    Aprendendo Axios

---
+++ 08-Axios/js/scripts.js
@@ -0,0 +1,49 @@
+// 01 - Instalação Axios
+console.log(axios);
+
+// 02 - primeiro request
+const getData = async() =>{
+    try {
+        const  response = await axios.get("https://jsonplaceholder.typicode.com/users",
+            // 04 - definindo headers
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            }
+        );
+        console.log(response);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getData();
+
+// 03 - imprimindo na tela
+const container = document.querySelector("#user-container");
+
+const printData = async () => {
+    const data = await getData();
+    data.forEach((user) => {
+        // criar o elemento
+        const div = document.createElement("div");
+        // Criando o nome do elemento
+        const nameElement = document.createElement("h2");
+        nameElement.textContent = user.name;
+
+        // Colocando o nome dentro da div criada
+        div.appendChild(nameElement);
+
+        // Colocar email do usuário
+        const emailElement = document.createElement("p");
+        emailElement.textContent = user.email;
+        div.appendChild(emailElement);
+
+        container.appendChild(div)
+
+    });
+};
+
+printData();
