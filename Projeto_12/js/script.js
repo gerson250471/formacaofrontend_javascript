728b8b5dda174950aa6ebb0fd30eb9b48b81dc86
Gerson Bernardo <gbds25@gmail.com>
2024-12-23 13:44:11 -0300

    Início do Projeto 12

---
+++ Projeto_12/js/script.js
@@ -0,0 +1,7 @@
+// Seleção de Elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+// Funcões
+
+// Eventos
