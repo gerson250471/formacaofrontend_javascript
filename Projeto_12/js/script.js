d21276fd1e8f2910a55d0e496d4f3bae18f215d9
Gerson Bernardo <gbds25@gmail.com>
2025-01-10 22:48:27 -0300

    Andamento do Projeto 12 parte 2

--- Projeto_12/js/script.js
+++ Projeto_12/js/script.js
@@ -2,6 +2,21 @@
 const generatePasswordButton = document.querySelector("#generate-password");
 const generatedPasswordElement = document.querySelector("#generated-password");
 
+// Novas Funcionalidades
+/* Abrir tabela com escolhas novas senhas */
+const openCloseGeneratorButton = document.querySelector("#open-generate-password")
+const generatePasswordContainer = document.querySelector("#generate-options")
+/* Opção do Tamanho */
+const lengthInput = document.querySelector("#length")
+/* Opção das letras */
+const lettersInput = document.querySelector("#letters")
+/* Opção de números */
+const numbersInput = document.querySelector("#numbers")
+/* Opção de Simbolos */
+const symbolsInput = document.querySelector("#symbols")
+/* Botão de copiar Senha */
+const copyPasswordButton = document.querySelector("#copy-password")
+
 // Funcões
 /* Gerar Letras */
 const getLetterLowerCase = () =>{
@@ -25,10 +40,27 @@ const getSymbol = () =>{
 
 const generatePassword = (getLetterLowerCase,getLetterUpperCase,getNumber,getSymbol) => {
     let password = ""
-    const passwordLengh = 10
-    const generators =[getLetterLowerCase,getLetterUpperCase,getNumber,getSymbol]
+    const passwordLengh = +lengthInput.value;
+    const generators =[]
 
-    for(i=0;i < passwordLengh;i = i + 4){
+    /* Obtendo o valor das opções escolhidas */
+    if(lettersInput.checked){
+        generators.push(getLetterLowerCase,getLetterUpperCase);
+    }
+
+    if(numbersInput.checked){
+        generators.push(getNumber);
+    }
+
+    if(symbolsInput.checked){
+        generators.push(getSymbol);
+    }
+
+    if(generators.length===0){
+        return;
+    }
+
+    for(i=0;i < passwordLengh;i = i + generators.length){
         generators.forEach(() =>{
             const randomValue = generators[Math.floor(Math.random() * generators.length)]();
 
@@ -44,4 +76,9 @@ const generatePassword = (getLetterLowerCase,getLetterUpperCase,getNumber,getSym
 // Eventos
 generatePasswordButton.addEventListener("click",() =>{
     generatePassword(getLetterLowerCase,getLetterUpperCase,getNumber,getSymbol);
+});
+
+/* Mostrar ou ocultar Caixa de Opções de Senhas */
+openCloseGeneratorButton.addEventListener("click", () => {
+    generatePasswordContainer.classList.toggle("hide");
 });
