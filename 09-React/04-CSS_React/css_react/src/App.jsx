daff6e2d23ba57542e6dd346f391af7531ceb087
Gerson Bernardo <gbds25@gmail.com>
2025-04-11 19:21:15 -0300

    Conclusão  do estudo de CSS no REACT

--- 09-React/04-CSS_React/css_react/src/App.jsx
+++ 09-React/04-CSS_React/css_react/src/App.jsx
@@ -3,8 +3,14 @@ import './App.css'
 
 // 02 - CSS de componente
 import MyComponent from './components/MyComponent'
+import Title from './components/Title';
 
 function App() {
+  // 04 - inline style dinâmico
+  const n = 15
+
+  // 05 - classe dinâmica
+  const redTitle = true;
 
   return (
     <div className='App'>
@@ -13,6 +19,15 @@ function App() {
       {/* 02 - css de component */}
       <MyComponent />
       <p>Pegou o CSS do componente</p>
+      {/* 03 - inline style */}
+      <p style={{color:'chocolate',padding:"25px",borderTop:"1px dotted blue"}}>Este elemento tem estilos inline</p>
+      {/* 04 - inline style dinâmico */}
+      <h2 style={ n > 10 ? {color:"purple"} : {color:"magenta"}}>CSS Dinâmico</h2>
+      <h2 style={ n > 20 ? {color:"purple"} : {color:"magenta"}}>CSS Dinâmico 2</h2>
+      {/* 05 - classe dinâmica */}
+      <h3 className= {redTitle ? "red-title" : "title"} >Este estilo vai ter uma classe</h3>
+      {/* 06 - css modules */}
+      <Title />
     </div>
   )
 }
