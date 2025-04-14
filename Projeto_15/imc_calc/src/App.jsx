3dffbef93a972e020146a97006504542cecccb9e
Gerson Bernardo <gbds25@gmail.com>
2025-04-14 17:26:04 -0300

    Início do Projeto 15

---
+++ Projeto_15/imc_calc/src/App.jsx
@@ -0,0 +1,31 @@
+import {data} from "./data/data"
+import { useState } from 'react'
+import ImcCalc from './components/ImcCalc'
+import ImcTable from "./components/ImcTable";
+import './App.css'
+
+function App() {
+  const calcImc = (e, height, weight) =>{
+    e.preventDefault();
+
+    console.log("Apertou o Botão")
+  }
+
+  const [imc,setImc] = useState("");
+  const [info,setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
+
+  return (
+    <>
+      <div className='container'>
+        {!imc ? (
+          <ImcCalc calcImc={calcImc} />
+        ) : (
+          <ImcTable />
+        ) }
+      </div>
+    </>
+  )
+}
+
+export default App
